92c58b8c4a597fcd363e2e2ada14b3d77812216f
Edward Kim <edk217@gmail.com>
2012-04-16 23:16:50 -0400

    add file cleaning to initialize method

--- event_reporter.rb
+++ event_reporter.rb
@@ -9,10 +9,15 @@ require "csv"
 # Class Definition
 class EventReporter	
 
+  INVALID_ZIPCODE = "00000"
+
   def initialize
     puts "EventReporter Initialized."
     filename = "event_attendees.csv"
     @file = CSV.open(filename, {:headers => true, :header_converters => :symbol})
+    cleanup_data
+    clean_filename = "event_attendees_clean.csv"
+    @clean_file = CSV.open(clean_filename, {:headers => true, :header_converters => :symbol})
   end
 
 
@@ -27,8 +32,7 @@ class EventReporter
       command = parts[0]
       case command
         when 'q' then puts "Goodbye!"
-        when 'print' then print_names
-        when 'printa' then print_all
+        when 'print' then print     
         when 'printh' then print_headers
         else puts "Sorry, I don't know how to #{command}"
       end     
@@ -39,22 +43,71 @@ class EventReporter
   def print_headers    
     #puts @file.gets.each.headers 
     
-    @file.gets.each do |header, data|
+    @clean_file.gets.each do |header, data|
       printf header.to_s + "\t"
     end
     # how do i insert tabs between headers?
   end
 
 
-  def print_names
-    @file.each do |line|
-      puts line[:first_name] + "\t" + line[:last_name]
-    end
+  #def print_names
+   # @file.each do |line|
+   #   puts line[:first_name] + "\t" + line[:last_name]
+   # end
 
-    @file.rewind
+   # @file.rewind
+  #end
+
+  def print
+   @clean_file.each do |line|
+     printf line[:regdate] + "\t" 
+     printf line[:first_name] + "\t"
+     printf line[:last_name] + "\t"
+     printf line[:email_address] + "\t"
+     printf line[:homephone] + "\t"
+     printf line[:street] + "\t"
+     printf line[:city] + "\t"
+     printf line[:state] + "\t"
+     printf line[:zipcode] + "\n"
+   end
+   @file.rewind
   end
 
-  def print_all
+
+  def cleanup_data
+    output = CSV.open("event_attendees_clean.csv", "w")
+    @file.each do |line|
+      if @file.lineno == 2
+        output << line.headers
+        output << line
+      else 
+        line[:street] = " " if line[:street].nil?
+        line[:city] = " " if line[:city].nil?
+        line[:state] = " " if line[:state].nil?        
+        line[:homephone] = clean_number(line[:homephone])
+        line[:zipcode] = clean_zipcode(line[:zipcode])
+        output << line
+      end
+    end
+  end
+
+  
+  def clean_zipcode(zipcode)
+    if zipcode.nil?
+      zipcode = INVALID_ZIPCODE
+    elsif
+      unless zipcode.length >= 5
+        zipcode = "0" + zipcode
+      end
+    else
+      # Do Nothing
+    end
+
+    return zipcode
+  end
+
+
+  def print_all_neat
     @file.each do |line|
       line.each do |header, data|
         printf data + "\t" #breaks when it hits null zip code
@@ -82,7 +135,7 @@ class EventReporter
   end
 
   def clean_number(original)
-  	  	@file.each do |line|
+  	@file.each do |line|
   		number = line[:homephone]
   		clean_number = number.delete(".")
   		clean_number = clean_number.delete(" ")
@@ -90,7 +143,7 @@ class EventReporter
   		clean_number = clean_number.delete("(")
   		clean_number = clean_number.delete(")")
   		return clean_number
-	end
+	  end
   end
 
    def rep_lookup
@@ -113,17 +166,7 @@ class EventReporter
     end
   end
 
-  def output_data
-    output = CSV.open("event_attendees_clean.csv", "w")
-    @file.each do |line|
-      if @file.lineno == 2
-      	output << line.headers
-      else 
-      	line[:homephone] = clean_number(line[:homephone])
-      	output << line
-      end
-    end
-  end
+  
 end
 
 # Script
